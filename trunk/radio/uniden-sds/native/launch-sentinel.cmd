86653f5eaedec5ae85037f04c44ebd1d869be099
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2023-03-01 23:38:55 +0000

    Added delay before invoking Sentinel to address what I think may be a race condition.

--- trunk/radio/uniden-sds/native/launch-sentinel.cmd
+++ trunk/radio/uniden-sds/native/launch-sentinel.cmd
@@ -3,4 +3,5 @@ set PROFILE=%2
 set SENTINELDATAFOLDER=C:\Users\KevinAndrewLipscomb\AppData\Local\Uniden\BCDx36HP_Sentinel.exe_Url_abqsk2nho02xj05iqogfzzs5adwkwlkr\1.0.0.0
 del %SENTINELDATAFOLDER%\user.config
 mklink /h %SENTINELDATAFOLDER%\user.config %SENTINELDATAFOLDER%\user---%INSTANCE%---%PROFILE%.config
+timeout 1
 "C:\Program Files (x86)\Uniden\BCDx36HP Sentinel\BCDx36HP_Sentinel.exe"
